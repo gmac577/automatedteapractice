842c8d814190def274cc5b91ff6ab28644ecc759
Gordon <gmccowey@riis.com>
2016-03-23 01:31:33 -0400

    Adding intitial file place holders for the step definition and page object files.

---
+++ features/step_definitions/initial_steps.rb
@@ -0,0 +1,2 @@
+##Initial file for step_definition page of project.
+#Please delete these comments and re-use this file.
---
+++ features/support/pages/initialClass.rb
@@ -0,0 +1,3 @@
+class Welcomepage
+
+	##This is a placeholder for the first page_object class file
